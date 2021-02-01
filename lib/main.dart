70cdc8975e8512b560521caddc7af0bc1d587d92
hann171 <hannw171@outlook.com>
2021-02-01 16:10:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //fungsi main, fungsi yang pertama kali dijalankan saat aplikasi dibuka
+  runApp(MyApp()); //fungsi main jalanin class MyApp
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Fundamental'),),
+        
+      ), //tampilan awal yang ditampilin MaterialApp
+    );
+  }
+}
